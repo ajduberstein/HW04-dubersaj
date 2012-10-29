94a63f6da850ba98c74df08b28141e4302ae525e
Andrew Duberstein <ajduberstein@gmail.com>
2012-10-29 12:35:27 -0400

    Fully functional quadtree

--- src/Quadtree.cpp
+++ src/Quadtree.cpp
@@ -25,43 +25,73 @@ void Quadtree::build(Entry* c, int n){
 		tmp->setEntry(&entries[i]);
 		subdivide(tmp,root);
 	}
-	delete entries;
+	delete[] entries;
 }
 
-void Quadtree::subdivide(Quadtree* inserted_point, Quadtree* current_point){
-	
-	if(inserted_point->getEntry()->y > current_point->getEntry()->y)
+void Quadtree::subdivide(Quadtree* inserted_point, Quadtree* current_point)
+{
+	//Substract x and y's, compare with .000001, return without doing anything
+	if (abs(inserted_point->getEntry()->x - current_point->getEntry()->x) > .00001 &&
+		abs(inserted_point->getEntry()->y - current_point->getEntry()->y) > .00001)
 	{
-		if(inserted_point->getEntry()->x <= current_point->getEntry()->x)
-			if(current_point->northWest == NULL){
-				current_point->northWest = inserted_point;
+		//North points	
+		if(inserted_point->getEntry()->y > current_point->getEntry()->y)
+		{
+			if(inserted_point->getEntry()->x <= current_point->getEntry()->x)
+			{
+				if(current_point->northWest == NULL)
+				{
+					current_point->setNorthWest(inserted_point);
+				}
+				else
+				{
+					subdivide(inserted_point, current_point->northWest);
+				}
 			}
 			else
-				subdivide(inserted_point, current_point->northWest);
-		else
-			if(current_point->northEast == NULL){
-				current_point->northEast = inserted_point;
+			{
+				if(current_point->northEast == NULL)
+				{
+					current_point->setNorthEast(inserted_point);
+				}
+				else
+				{
+					subdivide(inserted_point, current_point->northEast);
+				}
+			}
+		}
+		//South points
+		else if(inserted_point->getEntry()->y <= current_point->getEntry()->y)
+		{
+			if(inserted_point->getEntry()->x <= current_point->getEntry()->x)
+			{
+				if(current_point->southWest == NULL)
+				{
+					current_point->setSouthWest(inserted_point);
+				}
+				else
+				{
+					subdivide(inserted_point, current_point->southWest);
+				}
 			}
 			else
-				subdivide(inserted_point, current_point->northEast);
-	}
-	else if(inserted_point->getEntry()->y <= current_point->getEntry()->y)
-	{
-		if(inserted_point->getEntry()->x <= current_point->getEntry()->x)
-			if(current_point->southWest == NULL){
-				current_point->southWest = inserted_point;
+			{
+				if(current_point->southEast == NULL)
+				{
+					current_point->setSouthEast(inserted_point);
+				}
+				else
+				{
+					subdivide(inserted_point, current_point->southEast);
+				}
 			}
-			else
-				subdivide(inserted_point, current_point->southWest);
-		else
-			if(current_point->southEast == NULL){
-				current_point->southEast = inserted_point;
-			}
-			else
-				subdivide(inserted_point, current_point->southEast);
+		}
+		else{
+			string s = "If I show up in the debugger, there's an error.";
+		}
 	}
 	else{
-		string s = "If I show up in the debugger, there's an error.";
+		//Do nothing
 	}
 }
 
